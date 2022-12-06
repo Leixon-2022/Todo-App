1226eacde930df6ad2df181553367ed2219fd098
Jeberholst <j_eberholst@hotmail.com>
2022-12-06 13:52:57 +0100

    Comments

--- js/app.js
+++ js/app.js
@@ -6,15 +6,24 @@ function addInitialTodos() {
   addTodoItem("Handla", "Glömde köpa ost på burk, till våra tacochips")
 }
 
+// När dokumentet laddats så kör vi funktion addInitialTodos
 document.addEventListener("load", addInitialTodos())
 
+// denna funktionen körs av input="submit", skickar iväg form
 function onFormSubmit() {
+
   const inputTitle = document.querySelector("input[name='todo-title']")
   const inputText = document.querySelector("input[name='todo-text']")
+  //skickar med värdena till funktionen addTodoItem
+  //värdena kommer från våra inputs, hämtas via element~.value
   addTodoItem(inputTitle.value, inputText.value)
 }
 
 function addTodoItem(title, text) {
+
+  // title = kommer från inputTitle.value
+  // text = kommer från inputText.value
+
   console.log('Add todo item')
 
   const targetElement = document.getElementById("todo-container")
@@ -24,16 +33,17 @@ function addTodoItem(title, text) {
   const newTodoDate = document.createElement("span")
   const newTodoText = document.createElement("p")
   const newBtnRemove = document.createElement("button")
-  const newBtnRemoveIcon = document.createElement("img") //denna
+  const newBtnRemoveIcon = document.createElement("img")
 
   newTodoTitle.innerText = title
   newTodoDate.innerText = `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`
   newTodoText.innerText = text
-  newBtnRemoveIcon.src = "../assets/ic_trash.svg" //denna
+  newBtnRemoveIcon.src = "../assets/ic_trash.svg"
 
-  newBtnRemove.appendChild(newBtnRemoveIcon) //lägg på knappen
+  newBtnRemove.appendChild(newBtnRemoveIcon)
 
   const determineCardColor = `card-color-${Math.floor(Math.random() * 3 + 1)}`
+  // class = "todo-item card-color-randomNummer(1-3)"
   newTodoItem.classList.add("todo-item", determineCardColor)
   newBtnRemove.classList.add("btn-remove-todo")
 
