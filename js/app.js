3dfbf5f6b24ce059ffc3a31ce22e3644f56b8f65
Jeberholst <j_eberholst@hotmail.com>
2022-12-06 13:40:52 +0100

    Changes

--- js/app.js
+++ js/app.js
@@ -1,32 +1,47 @@
 console.log('Script app.js connected');
 
+function addInitialTodos() {
+  addTodoItem("Köket", "Diska vår disk för hand")
+  addTodoItem("Vardagsrum", "Städa under soffan (dammigt)")
+  addTodoItem("Handla", "Glömde köpa ost på burk, till våra tacochips")
+}
 
-function addTodoItem() {
-  console.log('onClick - Add todo item')
+document.addEventListener("load", addInitialTodos())
+
+function onFormSubmit() {
+  const inputTitle = document.querySelector("input[name='todo-title']")
+  const inputText = document.querySelector("input[name='todo-text']")
+  addTodoItem(inputTitle.value, inputText.value)
+}
+
+function addTodoItem(title, text) {
+  console.log('Add todo item')
+
   const targetElement = document.getElementById("todo-container")
 
   const newTodoItem = document.createElement("div")
-  newTodoItem.classList.add("todo-item")
-
-  // Add h2-tag to newTodoItem, a title
   const newTodoTitle = document.createElement("h2")
-  newTodoTitle.innerText = "Todo title"
-  newTodoItem.appendChild(newTodoTitle)
-
-  // Add p-tag, our thing that we should do
+  const newTodoDate = document.createElement("span")
   const newTodoText = document.createElement("p")
-  newTodoText.innerText = "Something we need to do"
-  newTodoItem.appendChild(newTodoText)
-
-  // Add button-tag, a button to remove "node" (newTodoItem)
   const newBtnRemove = document.createElement("button")
-  newBtnRemove.innerText = "Remove"
+  const newBtnRemoveIcon = document.createElement("img") //denna
+
+  newTodoTitle.innerText = title
+  newTodoDate.innerText = `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`
+  newTodoText.innerText = text
+  newBtnRemoveIcon.src = "../assets/ic_trash.svg" //denna
+
+  newBtnRemove.appendChild(newBtnRemoveIcon) //lägg på knappen
+
+  const determineCardColor = `card-color-${Math.floor(Math.random() * 3 + 1)}`
+  newTodoItem.classList.add("todo-item", determineCardColor)
   newBtnRemove.classList.add("btn-remove-todo")
+
   newBtnRemove.addEventListener("click", function () {
     console.log('onClick - Remove todo item')
     newTodoItem.remove();
   })
-  newTodoItem.appendChild(newBtnRemove)
 
+  newTodoItem.append(newTodoTitle, newTodoDate, newTodoText, newBtnRemove)
   targetElement.appendChild(newTodoItem)
 }
---
+++ readme.md
@@ -0,0 +1,4 @@
+# ReadME
+
+### Kommandon i kod
+alt + pilar (upp | ner ) för att flytta rad ni står på upp | ner
