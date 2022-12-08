dca46c0632f6f8e4eb2c8c9505cd7447fa71c41f
Jeberholst <j_eberholst@hotmail.com>
2022-12-08 13:02:56 +0100

    App as an object with functions

--- js/app.js
+++ js/app.js
@@ -1,57 +1,114 @@
 console.log('Script app.js connected');
 
-function addInitialTodos() {
-  addTodoItem("Köket", "Diska vår disk för hand")
-  addTodoItem("Vardagsrum", "Städa under soffan (dammigt)")
-  addTodoItem("Handla", "Glömde köpa ost på burk, till våra tacochips")
+// Ett "Top level" object, ett globalt "App"-objekt (egendefinerat)
+
+const App = {
+  listOfTodos: [], //vår lista av todos ska in här
+  elements: { //Våra "main"-element via ska arbeta mot stoppar vi in här
+    container: document.getElementById("todo-container")
+  },
+  addInitialTodos: function () { //Vi skapar upp 3 initiala Todos som ska renderas vid start
+    this.listOfTodos.push(
+      createTodoItem("Köket", "Diska vår disk för hand", Date.now() + 1),
+      createTodoItem("Vardagsrum", "Städa under soffan (dammigt)", Date.now() + 2),
+      createTodoItem("Handla", "Glömde köpa ost på burk, till våra tacochips", Date.now() + 3),
+    )
+  },
+  create: function () { //Våran funktion för att skapa en Todo
+    const inputTitle = document.querySelector("input[name='todo-title']")
+    const inputText = document.querySelector("input[name='todo-text']")
+    //skickar med värdena till funktionen addTodoItem
+    //värdena kommer från våra inputs, hämtas via element~.value
+    this.listOfTodos.push(createTodoItem(inputTitle.value, inputText.value))
+    this.render()
+  },
+  update: function (id) { //Våran funktion för att update en Todo
+    let findItemIndex = this.listOfTodos.findIndex(item => item.id == id)
+    this.listOfTodos[findItemIndex].checked = !this.listOfTodos[findItemIndex].checked
+
+    this.render()
+  },
+  remove: function (id) { //Våran funktion för att ta bort en Todo
+    this.render()
+  },
+  render: function () { //Våran funktion för rendera (rita upp i vår HTML i detta fall) alla Todos
+
+    this.elements.container.innerHTML = "" // Vi tömmer våran nuvarande "container"
+
+    // Vi loopar igenom alla items i listOfTodos
+    this.listOfTodos.forEach((item) => {
+
+      const newTodoItem = document.createElement("div")
+      const newTodoTitle = document.createElement("h2")
+      const newTodoDate = document.createElement("span")
+      const newTodoText = document.createElement("p")
+      const newBtnRemove = document.createElement("button")
+      const newBtnCheck = document.createElement("button") //* Denna
+      const newBtnRemoveIcon = document.createElement("img")
+
+      newTodoTitle.innerText = item.title
+      newTodoDate.innerText = `${item.id}`
+      newTodoText.innerText = item.text
+      newBtnRemoveIcon.src = "../assets/ic_trash.svg"
+      newBtnCheck.innerText = item.checked ? "Done" : "Mark Done"
+
+      newBtnRemove.appendChild(newBtnRemoveIcon)
+
+      const determineCardColor = `card-color-${item.colorIndex}`
+
+      newTodoItem.classList.add("todo-item", determineCardColor)
+      newBtnRemove.classList.add("btn-remove-todo")
+      item.checked ? newTodoItem.classList.add("todo-checked") : null
+
+      newBtnRemove.addEventListener("click", function () {
+        console.log('onClick - Remove todo item')
+        App.remove(item.id);
+      })
+
+      newBtnCheck.addEventListener("click", function() {
+        App.update(item.id)
+      })
+
+      newTodoItem.append(
+        newTodoTitle,
+        newTodoDate,
+        newTodoText,
+        newBtnRemove,
+        newBtnCheck
+      )
+
+      this.elements.container.appendChild(newTodoItem)
+
+    })
+  }
+
 }
 
-// När dokumentet laddats så kör vi funktion addInitialTodos
-document.addEventListener("load", addInitialTodos())
+function createTodoItem(suppliedTitle, suppliedText, suppledId) {
+
+  //slumpmässig siffra mellan 1-3
+  const ranColIndex = Math.floor(Math.random() * 3 + 1)
+
+  // elvis-operator ?:    värde/vilkor ? true/finns : false/finns inte
+  return {
+    id: suppledId ? suppledId : Date.now(),
+    title: suppliedTitle,
+    text: suppliedText,
+    colorIndex: ranColIndex,
+    checked: false
+  }
+}
 
 // denna funktionen körs av input="submit", skickar iväg form
 function onFormSubmit() {
-
-  const inputTitle = document.querySelector("input[name='todo-title']")
-  const inputText = document.querySelector("input[name='todo-text']")
-  //skickar med värdena till funktionen addTodoItem
-  //värdena kommer från våra inputs, hämtas via element~.value
-  addTodoItem(inputTitle.value, inputText.value)
+  App.create()
 }
 
-function addTodoItem(title, text) {
 
-  // title = kommer från inputTitle.value
-  // text = kommer från inputText.value
+App.addInitialTodos()
+App.render()
 
-  console.log('Add todo item')
-
-  const targetElement = document.getElementById("todo-container")
-
-  const newTodoItem = document.createElement("div")
-  const newTodoTitle = document.createElement("h2")
-  const newTodoDate = document.createElement("span")
-  const newTodoText = document.createElement("p")
-  const newBtnRemove = document.createElement("button")
-  const newBtnRemoveIcon = document.createElement("img")
-
-  newTodoTitle.innerText = title
-  newTodoDate.innerText = `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`
-  newTodoText.innerText = text
-  newBtnRemoveIcon.src = "../assets/ic_trash.svg"
-
-  newBtnRemove.appendChild(newBtnRemoveIcon)
-
-  const determineCardColor = `card-color-${Math.floor(Math.random() * 3 + 1)}`
-  // class = "todo-item card-color-randomNummer(1-3)"
-  newTodoItem.classList.add("todo-item", determineCardColor)
-  newBtnRemove.classList.add("btn-remove-todo")
-
-  newBtnRemove.addEventListener("click", function () {
-    console.log('onClick - Remove todo item')
-    newTodoItem.remove();
-  })
-
-  newTodoItem.append(newTodoTitle, newTodoDate, newTodoText, newBtnRemove)
-  targetElement.appendChild(newTodoItem)
+function logApp() {
+  console.log(App)
+  console.table(App.listOfTodos)
 }
